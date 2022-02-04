5efe51c943d247d42ed20541c1f4e70b418e0570
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-04 12:43:35 +0300

    initial commit and Hello World

---
+++ C#101/Ders Notları/Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Console_Program {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " " + surname);
+        }
+    }
+}
