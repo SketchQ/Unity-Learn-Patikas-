cdbb9f3e3a2c3d4dfe12c0fe2692b56c88d120a9
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-04 17:01:53 +0300

    Done

--- C#101/Ders Notları/Hata Yonetimi/Program.cs
+++ C#101/Ders Notları/Hata Yonetimi/Program.cs
@@ -1,9 +1,60 @@
 ﻿using System;
 
-namespace Hata_Yonetimi {
-    class program {
-        public static void Main(string[] args) {
-            
+namespace Hata_Yonetimi
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int sayi = Convert.ToInt32(Console.ReadLine()); // eğer sayı girilmezse hata verir.
+                Console.WriteLine("Your number is: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+            finally
+            {   // Hata olsun olmasın kullanıcıya bu mesajı verir
+                Console.WriteLine("Program finished");
+
+            }
+
+
+            try
+            {
+                int b = int.Parse(null); // Arayüzde null değerini girerse hata verir.
+                int a = int.Parse("abc"); // Arayüzde string değerini girerse hata verir.
+                int c = int.Parse("-200000000000"); // Overflow hatası verir. 
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex) {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Program finished");
+            }
+
+
+
+
+
+
         }
     }
 }
