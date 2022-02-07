56e929f9065852175a4834d7ff5869effd2778a2
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-07 16:25:13 +0300

    Done

--- C#101/Ders Notları/switch-case/Program.cs
+++ C#101/Ders Notları/switch-case/Program.cs
@@ -5,7 +5,40 @@ namespace Switch_Case {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Hello, World!");
+            int month = DateTime.Now.Month;
+
+            // Expression
+            switch (month) {
+                case 1 :
+                    Console.WriteLine("Ocak ayındasınız.");
+                    break;
+                case 2 :
+                    Console.WriteLine("Şubat ayındayısınız.");
+                    break;
+                case 6 :
+                    Console.WriteLine("Haziran ayındasınız.");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri!!");
+                    break;
+            }
+
+
+            // Switch case içerisinde aşağıdaki gibi birden fazla condition verilebilinir.    
+            switch (month) {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Aylarındasınız.");
+                    break;
+                case 3 :
+                case 4 :
+                case 5 :
+                    Console.WriteLine("Bahar Aylarındasınız.");
+                    break;
+                default :
+                break;
+            }
         }
     }
 }
