ecfd605b5ee4174493b0e41137981ec1afefc656
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-07 19:34:13 +0300

    Done

--- C#101/Ders Notları/Diziler/Array_Sınıfı_metotları/Program.cs
+++ C#101/Ders Notları/Diziler/Array_Sınıfı_metotları/Program.cs
@@ -6,7 +6,64 @@ namespace Array_Class_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Sort Method
+            // Yeni diziyi tanımlamak
+            int[] numberArr = {23,12,4,86,72,3,11,17};
+            
+            Console.WriteLine("UnSorted list : ");
+            
+            // Sıralama olmadan console'a yazdıralım.
+            foreach (var num in numberArr)
+            {
+                Console.Write(num + "," );
+            }
+            
+            Console.WriteLine("\nSorted List :");
+            // Array.Sort(arr) --> Metotu
+            Array.Sort(numberArr);
+            // Sıralı bir şekilde console'a yazdırma
+            foreach (var num in numberArr)
+            {
+                Console.Write(num + ",");
+            }
+
+            // Clear Methodu
+            Console.WriteLine("\n****** Array Clear Method ******");
+            // Dizideki elemanları kullanarak 2. indisten itibaren 2 tane elemana 0 atar.
+            Array.Clear(numberArr,2,2);
+
+            foreach (var item in numberArr)
+            {
+                Console.Write(item + ",");        
+            }
+
+            // Reverse Methodu
+            Console.WriteLine("\n***** Reverse Method *****");
+
+            Array.Reverse(numberArr);
+
+            foreach (var item in numberArr)
+            {
+                Console.Write(item + ",");
+            }
+
+            // IndexOf Method
+            Console.WriteLine("\n***** IndexOf Method *****");
+
+            Array.IndexOf(numberArr,23);
+            
+            Console.WriteLine(Array.IndexOf(numberArr,23));
+
+            // Resize Method
+            Console.WriteLine("***** Resize Method *****");
+            // diziye yeni bir boyut atar.
+            Array.Resize<int>(ref numberArr, 9);
+            numberArr[8] = 99;
+
+            foreach (var item in numberArr)
+            {   
+                Console.Write(item + ",");
+            }
         }
     }
 }
