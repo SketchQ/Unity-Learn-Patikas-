111efe718a23eb990206ae0e80dfc79acf5fbcb1
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-07 17:43:16 +0300

    initial

---
+++ C#101/Ders Notları/Diziler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diziler 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
