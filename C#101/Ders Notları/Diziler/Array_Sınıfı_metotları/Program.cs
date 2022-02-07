cc2716c8b5de01b88b5e8dec9e624593599f6628
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-07 19:51:44 +0300

    BirthdayCakeCandles Function Added

--- C#101/Ders Notları/Diziler/Array_Sınıfı_metotları/Program.cs
+++ C#101/Ders Notları/Diziler/Array_Sınıfı_metotları/Program.cs
@@ -4,20 +4,37 @@ namespace Array_Class_methods
 {
     internal class Program
     {
+
+        // HackerRank BirthdayCakeCandles Function
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int max = candles.Max();
+            int count = 0;
+            foreach (int candle in candles)
+            {
+                if (candle == max)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
         static void Main(string[] args)
         {
             // Sort Method
             // Yeni diziyi tanımlamak
-            int[] numberArr = {23,12,4,86,72,3,11,17};
-            
+            int[] numberArr = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
             Console.WriteLine("UnSorted list : ");
-            
+
             // Sıralama olmadan console'a yazdıralım.
             foreach (var num in numberArr)
             {
-                Console.Write(num + "," );
+                Console.Write(num + ",");
             }
-            
+
             Console.WriteLine("\nSorted List :");
             // Array.Sort(arr) --> Metotu
             Array.Sort(numberArr);
@@ -30,11 +47,11 @@ namespace Array_Class_methods
             // Clear Methodu
             Console.WriteLine("\n****** Array Clear Method ******");
             // Dizideki elemanları kullanarak 2. indisten itibaren 2 tane elemana 0 atar.
-            Array.Clear(numberArr,2,2);
+            Array.Clear(numberArr, 2, 2);
 
             foreach (var item in numberArr)
             {
-                Console.Write(item + ",");        
+                Console.Write(item + ",");
             }
 
             // Reverse Methodu
@@ -50,9 +67,9 @@ namespace Array_Class_methods
             // IndexOf Method
             Console.WriteLine("\n***** IndexOf Method *****");
 
-            Array.IndexOf(numberArr,23);
-            
-            Console.WriteLine(Array.IndexOf(numberArr,23));
+            Array.IndexOf(numberArr, 23);
+
+            Console.WriteLine(Array.IndexOf(numberArr, 23));
 
             // Resize Method
             Console.WriteLine("***** Resize Method *****");
@@ -61,9 +78,29 @@ namespace Array_Class_methods
             numberArr[8] = 99;
 
             foreach (var item in numberArr)
-            {   
+            {
                 Console.Write(item + ",");
             }
+
+            // HackerRank Problems
+            Console.WriteLine("\n***** HackerRank Problems *****");
+
+            // You are in charge of the cake for a child's birthday. 
+            // You have decided the cake will have one candle for each year of their total age.
+            // They will only be able to blow out the tallest of the candles.
+            // Count how many candles are tallest.
+            Console.WriteLine("Enter the size of the array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            List<int> candles = new List<int>(size);
+
+            for (int i = 0; i < size ; i++)
+            {
+                Console.WriteLine("Enter the height of the {0} candle : ", i + 1);
+                candles.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            int result = Program.birthdayCakeCandles(candles);
+            Console.WriteLine("The number of tallest candles are : " + result);
+
         }
     }
 }
