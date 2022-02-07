2ed7c39b22f834d2cf25e34198caa5ca2db99153
Erim Serdönmez <eserdnmez@gmail.com>
2022-02-07 17:36:03 +0300

    Done

--- C#101/Ders Notları/Donguler-While-ForEach/Program.cs
+++ C#101/Ders Notları/Donguler-While-ForEach/Program.cs
@@ -6,7 +6,42 @@ namespace Donguler_While_ForEach
     {
         static void Main(string[] args)
         {
-            
+            // while
+            // 1'den başlayarak console'dan girilen sayıya kadar (Sayı dahil) ortalama hesaplayıp console'a yazdıran program
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            int sum = 0;
+
+            while (count <= number)
+            {
+                // Kod bloğu
+                sum += count;
+                count++;
+            }
+
+            Console.WriteLine(sum / number);
+
+            Console.WriteLine("************");
+
+            // 'a' dan 'z' ye kadar tüm harfleri console'a yazdır.
+            char ch = 'a';
+
+            while (ch < 'z')
+            {
+                Console.Write(ch);
+                ch++;
+            }
+
+
+            Console.WriteLine("\n****** Foreach *******");
+            // Dizi tanımlama
+            string[] cars = {"BMW", "Ford","Toyota","Nissan"};
+
+            foreach (var car in cars)
+            {   
+                Console.WriteLine(car);
+            }    
         }
     }
 }
